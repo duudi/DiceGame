294c40156f33fc03c4e7d395abcded556af461fc
sds2001 <seth.d.schroeder@gmail.com>
2017-12-06 20:14:06 +0000

    Player.cs: created Player class with members

---
+++ DiceGame/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace DiceGame
+{
+    class Player
+    {
+        public int Number { get; set; }
+        public int CurrentSquare { get; set; }
+        public int Score { get; set; }
+        public bool GoingBackwards { get; set; }
+        public Point Coords = new Point();
+    }
+}
