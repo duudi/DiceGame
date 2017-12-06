6101ce6fc367e253aa9ccad3c2ad820c94dc2d7f
sds2001 <seth.d.schroeder@gmail.com>
2017-12-06 18:25:51 +0000

    Message.cs: unnecessary variable declarations. Also multi-line comment by using "/* bla bla */".

--- DiceGame/Message.cs
+++ DiceGame/Message.cs
@@ -11,23 +11,27 @@ namespace DiceGame
         public static string path = Environment.CurrentDirectory + @"\Data\"; // Define a string variable called path and set it to /bin/Debug/data (where the text files are stored)
         public static string StartMessage() // StartMessage method
         {
-            string messageText = File.ReadAllText(path + "welcomemessage.txt"); // Read in all the text from welcomemessage.txt
-            return messageText; // Return the text from messagetext.txt so it can be displayed
+            // Read in all the text from welcomemessage.txt. Return the text from messagetext.txt so it can be displayed
+            return File.ReadAllText(path + "welcomemessage.txt");
         }
         public static string DoubleDiceMessage() // DoubleDiceMessage method
         {
             string tempText = File.ReadAllText(path + "doubledicemessage.txt"); // Read in all the text from doubledicemessage.txt into a temporary variable
             if (tempText.Contains("*")) // If the text file contains an asterisk
             {
-                string diceTotal = (Library.GlobalVariables.diceValue1 + Library.GlobalVariables.diceValue2).ToString(); // Calculate the total of the two dice and store it in a variable
-                string messageText = tempText.Replace("*", diceTotal); // The text in the .txt file has an asterisk where the variable should be inserted, this code replaces the asterisk with the total of the dice
-                return messageText; // Return the replaced text so it can be displayed
+                string diceTotal = (Library.GlobalVariables.diceValue1 + Library.GlobalVariables.diceValue2).ToString(); // Calculate the total o
+
+                /* The text in the .txt file has an asterisk where the variable should be inserted, this code replaces
+                the asterisk with the total of the dicef the two dice and store it in a variable.
+                Return the replaced text so it can be displayed */
+
+                return tempText.Replace("*", diceTotal);
             }
             else // If the text file doesn't contain an asterisk
             {
                 return tempText; // Return the text from doubledicemessage.txt without replacing anything
             }
-            
+
         }
         public static string WinMessage() // WinMessage method
         {
